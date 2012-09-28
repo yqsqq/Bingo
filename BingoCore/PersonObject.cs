e4998250c548aa04ca7fd882223fcc31eeeb8f65
xuehai <xhzeng@thoughtworks.com>
2012-09-28 14:39:09 +0800

    aa

---
+++ BingoCore/PersonObject.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace BingoCore
+{
+	public class PersonObject
+	{
+		public PersonObject ()
+		{
+		}
+
+		public string getRole()
+		{
+			return "my_role";
+		}
+	}
+}
+
---
+++ build.sh
@@ -0,0 +1,2 @@
+/Applications/MonoDevelop.app/Contents/MacOS/mdtool build
+nunit-console BingoCoreTest/bin/Debug/BingoCoreTest.dll
