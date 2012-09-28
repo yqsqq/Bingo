0634c03ecb79a092603284544be6edaea4011603
xuehai <xhzeng@thoughtworks.com>
2012-09-28 15:33:22 +0800

    aa

---
+++ Test/Test.cs
@@ -0,0 +1,16 @@
+
+using System;
+using NUnit.Framework;
+
+namespace Test
+{
+	[TestFixture]
+	public class Test
+	{
+		[Test]
+		public void Pass ()
+		{
+			Assert.True (true);
+		}
+	}
+}
--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 /Applications/MonoDevelop.app/Contents/MacOS/mdtool build
 nunit-console BingoCoreTest/bin/Debug/BingoCoreTest.dll
-/Developer/MonoTouch/usr/bin/mtouch -sdk="6.0" -sim Bingo4iPhoneTest/bin/iPhoneSimulator/Debug/Bingo4iPhoneTest.app/ Bingo4iPhoneTest/bin/iPhoneSimulator/Debug/Bingo4iPhoneTest.exe
-/Developer/MonoTouch/usr/bin/mtouch -launchsim Bingo4iPhoneTest/bin/iPhoneSimulator/Debug/Bingo4iPhoneTest.app/
+java -jar mtouch_adapter.jar
