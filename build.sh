e76998efdbe1020aa5117150e186d58e127919cb
xuehai <xhzeng@thoughtworks.com>
2012-10-12 09:50:50 +0800

    aa

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+rm -fr Bingo4iPhone/bin/iPhoneSimulator/*
+rm -fr BingoCore/bin/*
+rm -fr BingoCoreTest/bin/*
+rm -fr Test/bin/iPhoneSimulator/*
+
 /Applications/MonoDevelop.app/Contents/MacOS/mdtool build
 nunit-console BingoCoreTest/bin/Debug/BingoCoreTest.dll
 java -jar mtouch_adapter.jar
